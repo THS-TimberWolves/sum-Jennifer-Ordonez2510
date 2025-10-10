44e970b7325a783a0b1e3c564fa93757ac484004
Jennifer-Ordonez2510 <ordonjen000@alpinesd.org>
2025-10-10 10:39:24 -0600

    Update for_summation.py

--- for_summation.py
+++ for_summation.py
@@ -1,1 +1,9 @@
 #for Summation code here
+# Ask user for a number
+num = int(input("Enter a number: "))
+
+total = 0
+for i in range(1, num + 1):
+    total += i
+  
+print("The sum from 1 to", num, "is:", total)
